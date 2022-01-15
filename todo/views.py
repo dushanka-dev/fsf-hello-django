d779a2ad7579281d49eb8f3fd51a2e9f8c692581
Dushanka Perera <dushankadev@gmail.com>
2022-01-15 14:11:21 +0000

    Create add item html and form. Create new add item obeject

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,4 @@
-# imports
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 # Create your views here.
@@ -11,3 +10,12 @@ def get_todo_list(request):
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
+
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Item.object.create(name=name, done=done)
+
+        return redirect('get_todo_list')
+    return render(request, 'todo/add_item.html')
